18d278270aa8f01a268fb8471508c700d8b51988
YeonguChoe <yeongu.choe@icloud.com>
2026-01-23 02:12:16 -0500

    created codegen.cpp

---
+++ codegen.cpp
@@ -0,0 +1,18 @@
+#include <map>
+#include "ast.h"
+#include "parser.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Value.h"
+
+static std::unique_ptr<llvm::LLVMContext> TheContext;
+static std::unique_ptr<llvm::IRBuilder<>> Builder;
+static std::unique_ptr<llvm::Module> TheModule;
+static std::map<std::string, llvm::Value *> NamedValues;
+
+llvm::Value *LogErrorV(const char *str) {
+    LogError(str);
+    return nullptr;
+}
+
